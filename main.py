f4666cfd0b3d578c4587fbf26ef4adfbac2caeaa
50ngwe <100713071+50ngwe@users.noreply.github.com>
2022-03-02 19:42:15 +0200

    Added a third GET

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{Color}")
-def read_item(Pick_Color: str, Available: Optional[str] = None):
-    return {"Car Color": Pick_Color, "Available": q}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("Pick a color")
+def read_item(Color2: int, Available: Optional[str] = None):
+    return {"Color": Color2, "Available": Available}
+
 
