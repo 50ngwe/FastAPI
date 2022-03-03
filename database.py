afc247d2d26a2ef8bb6e7e33ede922414be7db28
50ngwe <100713071+50ngwe@users.noreply.github.com>
2022-03-03 17:19:56 +0200

    Added filters

---
+++ database.py
@@ -0,0 +1,31 @@
+import psycopg2
+from psycopg2 import Error
+from psycopg2.extras import RealDictCursor
+import json
+
+def conn():
+    return psycopg2.connect(database="Cars", user="techadmin@test-fast-api-db", password="Mustwork!!", host="test-fast-api-db.postgres.database.azure.com", port="5432")
+
+def fetch_one(field, value):
+    where_q = field+"='"+value+"'"
+    connection = conn()
+    cursor = connection.cursor(cursor_factory=RealDictCursor)
+    cursor.execute('SELECT item_id, available, model, trim_level, color, price FROM public."Cars_Inventory" WHERE '+where_q+';')
+    rows = cursor.fetchall()
+    return handle_results(rows)
+    
+def fetch_all():
+    connection = conn()
+    cursor = connection.cursor(cursor_factory=RealDictCursor)
+    cursor.execute('SELECT item_id, available, model, trim_level, color, price FROM public."Cars_Inventory";')
+    rows = cursor.fetchall()
+    return handle_results(rows)
+
+def handle_results(rows):
+    columns = ('item_id', 'available', 'model', 'trim_level', 'color', 'price')
+    results = []
+    for row in rows:
+        results.append(dict(zip(columns, row)))
+
+    #return json.dumps(results, indent=2)
+    return rows
--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from typing import Optional
-
 from fastapi import FastAPI
+import psycopg2
+from psycopg2 import Error
+import database 
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
+    return {"Hello": "App"}
+    
+@app.get("/items/")
+def read_item():
+    results = database.fetch_all()
+    return results
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: str, q: Optional[str] = None):
+    results = database.fetch_one("item_id", item_id)
+    return results
+
+@app.get("/items/{q_field}/{q_val}")
+def read_item(q_field: str, q_val: str, q: Optional[str] = None):
+    results = database.fetch_one(q_field, q_val)
+    return results
 
 @app.get("Pick a color")
 def read_item(Color2: str, Available: Optional[str] = None):
