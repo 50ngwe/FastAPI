10228ed12b4c6f69a8542647918eec71c1085d1a
50ngwe <100713071+50ngwe@users.noreply.github.com>
2022-03-08 23:12:50 +0200

    Changed server

--- database.py
+++ database.py
@@ -22,12 +22,14 @@ def fetch_all():
     rows = cursor.fetchall()
     return handle_results(rows)
 
-def update_one(reserve_id):
-    reserve = reserve_id
+def update_one(field, value):
+    where_q = field+"='"+value+"'"
     connection = conn()
     cursor = connection.cursor(cursor_factory=RealDictCursor)
-    cursor.execute('UPDATE public."Cars_Inventory" SET available="test" WHERE item_id='+reserve+';')
-    connection.commit()
+    cursor.execute('UPDATE public."Cars_Inventory" SET available="test" WHERE '+where_q+';')
+    cursor.execute('SELECT item_id, available, model, trim_level, color, price FROM public."Cars_Inventory" WHERE '+where_q+';')
+    rows = cursor.fetchall()
+    return handle_results(rows)
 
 def handle_results(rows):
     columns = ('item_id', 'available', 'model', 'trim_level', 'color', 'price')
